61b1e141cc4659c10426413d4a5a734d5f532bdd
Kavitha KC <kavithaprajod@gmail.com>
2022-08-03 15:05:18 +0800

    add controller list

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  # get 'lists/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'lists', to: 'lists#index'
+
+  get 'lists/new', to: 'lists#new'
+  post 'lists', to: 'lists#create'
+
+  get 'lists/:id', to: 'lists#show'
+
 end
